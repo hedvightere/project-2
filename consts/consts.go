5a90a09425a1b6aebfcf27ca17327d7bb31873db
hedvightere <hedvig88xoxo@gmail.com>
2024-07-16 14:03:59 +0530

    Update consts.go

--- consts/consts.go
+++ consts/consts.go
@@ -13,11 +13,11 @@ import (
 
 const (
 	// TODO: choose a human-readable part for your hyperchain
-	HRP = ""
+	HRP = "jackman"
 	// TODO: choose a name for your hyperchain
-	Name = ""
+	Name = "JACKCHAIN"
 	// TODO: choose a token symbol
-	Symbol = ""
+	Symbol = "JK"
 )
 
 var ID ids.ID
